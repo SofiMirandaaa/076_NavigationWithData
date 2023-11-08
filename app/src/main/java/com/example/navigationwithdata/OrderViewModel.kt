ada092eea9bcac9bd0877cd860c78449abc80cdc
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-11-09 02:52:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navigationwithdata/OrderViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.navigationwithdata
+
+import androidx.lifecycle.ViewModel
+import com.example.navigationwithdata.data.OrderUIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.text.NumberFormat
+
+private const val Harga_Per_Cup = 3000
+
+class OrderViewModel: ViewModel() {
+    private val _stateUI = MutableStateFlow(OrderUIState())
+    val stateUI: StateFlow<OrderUIState> = _stateUI.asStateFlow()
+
+    fun setJumlah(jmlEsJumbo:Int) {
+        _stateUI.update { stateSaatIni ->
+            stateSaatIni.copy(
+                jumlah = jmlEsJumbo,
+                harga = hitungHarga(jumlah = jmlEsJumbo)
+            )
+        }
+    }
+    fun setRasa(rasaPilihan: String){
+        _stateUI.update { stateSaatIni->
+            stateSaatIni.copy(rasa = rasaPilihan)
+        }
+    }
+
+    fun resetOrder(){
+        _stateUI.value = OrderUIState()
+    }
+
+    private fun hitungHarga(
+        jumlah: Int = _stateUI.value.jumlah,
+    ): String {
+        val kalkulasiHarga = jumlah * Harga_Per_Cup
+
+        return NumberFormat.getNumberInstance().format(kalkulasiHarga)
+    }
+}
---
+++ app/src/main/java/com/example/navigationwithdata/data/SumberData.kt
@@ -0,0 +1,13 @@
+package com.example.navigationwithdata.data
+
+import com.example.navigationwithdata.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.original,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lychee
+    )
+}
