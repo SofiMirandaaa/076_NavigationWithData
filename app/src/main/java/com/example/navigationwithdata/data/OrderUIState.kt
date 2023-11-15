d38d6f1f8e486ce911a55a8b4ecd9e18ade38207
Sofi Miranda <sofi.miranda.ft21@mail.umy.ac.id>
2023-11-15 23:56:11 +0700

    menambahkan resources data class

--- app/src/main/java/com/example/navigationwithdata/data/OrderUIState.kt
+++ app/src/main/java/com/example/navigationwithdata/data/OrderUIState.kt
@@ -3,5 +3,8 @@ package com.example.navigationwithdata.data
 data class OrderUIState(
     val jumlah: Int = 0,
     val rasa: String = "",
-    val harga: String = ""
+    val harga: String = "",
+    val nama: String = "",
+    val noTelp: String = "",
+    val alamat: String = ""
 )
